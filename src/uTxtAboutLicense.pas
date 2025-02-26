d09de3b3b218fe2c13fcd7a6b152d54ea9653e82
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-26 21:08:44 +0100

    copied license text unit

---
+++ src/uTxtAboutLicense.pas
@@ -0,0 +1,69 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Link Website Generator
+///
+/// Copyright 2025 Patrick Premartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://linkwebsitegenerator.olfsoftware.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Link-Website-Generator
+///
+/// ***************************************************************************
+/// File last update : 2025-02-26T20:46:18.000+01:00
+/// Signature : be7476d40eb02a98fe829a3670d0be2dee077215
+/// ***************************************************************************
+/// </summary>
+
+unit uTxtAboutLicense;
+
+interface
+
+function GetTxtAboutLicense(const Language: string;
+  const Recursif: boolean = false): string;
+
+implementation
+
+// For the languages codes, please use 2 letters ISO codes
+// https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+
+uses
+  System.SysUtils,
+  uConsts;
+
+const // TODO : mettre à jour la licence d'utilisation du programme
+  CTxtEN = 'your EULA (end user license agreement)';
+  CTxtFR = '';
+
+function GetTxtAboutLicense(const Language: string;
+  const Recursif: boolean): string;
+var
+  lng: string;
+begin
+  lng := Language.tolower;
+  if (lng = 'en') then
+    result := CTxtEN
+  else if (lng = 'fr') then // France
+    result := CTxtFR
+  else if not Recursif then
+    result := GetTxtAboutLicense(CDefaultLanguage, true)
+  else
+    raise Exception.Create('Unknow license for language "' + Language + '".');
+end;
+
+end.
