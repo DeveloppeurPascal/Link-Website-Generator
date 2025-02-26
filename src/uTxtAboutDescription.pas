88484e6db160b66fc3e75baf608328bd0f313871
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-26 21:08:56 +0100

    copied project description unit

---
+++ src/uTxtAboutDescription.pas
@@ -0,0 +1,70 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Link Website Generator
+///
+/// Copyright 2025 Patrick Premartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://linkwebsitegenerator.olfsoftware.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Link-Website-Generator
+///
+/// ***************************************************************************
+/// File last update : 2025-02-26T20:46:26.000+01:00
+/// Signature : 91071f3e305f5e569280f5d1dec3bcf1d3a6f7bb
+/// ***************************************************************************
+/// </summary>
+
+unit uTxtAboutDescription;
+
+interface
+
+function GetTxtAboutDescription(const Language: string;
+  const Recursif: boolean = false): string;
+
+implementation
+
+// For the languages codes, please use 2 letters ISO codes
+// https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+
+uses
+  System.SysUtils,
+  uConsts;
+
+const // TODO : mettre à jour le descriptif du programme
+  CTxtEN = 'comment for about box';
+  CTxtFR = '';
+
+function GetTxtAboutDescription(const Language: string;
+  const Recursif: boolean): string;
+var
+  lng: string;
+begin
+  lng := Language.tolower;
+  if (lng = 'en') then
+    result := CTxtEN
+  else if (lng = 'fr') then // France
+    result := CTxtFR
+  else if not Recursif then
+    result := GetTxtAboutDescription(CDefaultLanguage, true)
+  else
+    raise Exception.Create('Unknow description for language "' +
+      Language + '".');
+end;
+
+end.
